fb383597e71212f9e4cb1d93de6f2cc4cf1db031
André Freitas <freitasandre38@gmail.com>
2021-11-01 21:12:41 -0300

    Created Project

---
+++ backend/src/main/java/com/devsuperior/afanalises/AfanalisesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.afanalises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfanalisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfanalisesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/afanalises/AfanalisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.afanalises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfanalisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
